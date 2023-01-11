2ca98d2a8382dd93a3d1102e941cca1cd59de473
SvetlanaMaslennikova <23481505+SvetlanaMaslennikova@users.noreply.github.com>
2023-01-11 21:46:26 +0300

    Add files via upload

---
+++ task_2.py
@@ -0,0 +1,29 @@
+# 2. *(вместо задачи 1) Доработать предыдущую функцию в num_translate_adv():
+# реализовать корректную работу с числительными, начинающимися с заглавной буквы
+# — результат тоже должен быть с заглавной. Например:
+# >>> num_translate_adv("One")
+# "Один"
+# >>> num_translate_adv("two")
+# "два"
+
+def num_translate_adv(n):
+    nums = {
+        "zero": "ноль",
+        "one": "один",
+        "two": "два",
+        "three": "три",
+        "four": "четыре",
+        "five": "пять",
+        "six": "шесть",
+        "seven": "семь",
+        "eight": "восемь",
+        "nine": "девять",
+        "ten": "десять"}
+    if n.istitle() and nums.get(n.lower()):
+        return nums.get(n.lower()).title()
+    else:
+        return nums.get(n)
+
+
+print(num_translate_adv("One"))
+print(num_translate_adv("two"))
